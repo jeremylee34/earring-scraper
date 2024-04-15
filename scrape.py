7004e22a7870fd336750420897e589e0b6d180be
Jeremy Lee <jeremy.lee6857@gmail.com>
2024-04-15 18:25:01 +1000

    first commit

--- scrape.py
+++ scrape.py
@@ -1,32 +1,8 @@
-import logging
-import logging.handlers
-import os
-
 import requests
 from bs4 import BeautifulSoup
 from email.message import EmailMessage
 import ssl
 import smtplib
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
-
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
-
 
 def my_function():
     sender_email = "dosiracbot@gmail.com"
@@ -53,8 +29,7 @@ def my_function():
         body += f"\n{result_str_final}"
         
         print(body)
-        send_email(subject, body, sender_email, receiver_emails, password)
-        return 'Sold out' if sold_out else 'In Stock'
+        # send_email(subject, body, sender_email, receiver_emails, password)
 
     else:
         print("Failed to scrape content.")
@@ -101,15 +76,4 @@ def send_email(subject, body, sender_email, receiver_emails, password):
         print('Email sent')
         
 
-
-if __name__ == "__main__":
-    # logger.info(f"Token value: {SOME_SECRET}")
-
-    stock_status = my_function()
-    logger.info(f'Status: {stock_status}')
-
-    # r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    # if r.status_code == 200:
-    #     data = r.json()
-    #     temperature = data["forecast"]["temp"]
-    #     logger.info(f'Weather in Berlin: {temperature}')
+my_function()
